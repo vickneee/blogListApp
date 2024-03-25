30a9590c09e38373ee549ca2a7f5d2235ecae544
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-03-26 01:21:58 +0200

    Exercise 4.2

---
+++ controllers/blogController.js
@@ -0,0 +1,29 @@
+const Blog = require("../models/blogModel");
+const express = require("express");
+
+const baseUrl = 'localhost:3001/';
+
+const app = express();
+
+const getAllBlogs = app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+const createBlog = app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+module.exports = {
+  getAllBlogs,
+  createBlog
+}
