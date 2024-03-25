4bf4495198d9667146eb88ce91504902656922ca
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-03-25 23:29:08 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// Purpose: Create a simple REST API for a blog list application
+import express from 'express'
+const app = express()
+import cors from 'cors'
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
