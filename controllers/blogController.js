d48bc6f0498182c7b9b572b2faadf08877911420
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-03-27 22:55:12 +0200

    APIs Refactoring the code

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -11,22 +11,28 @@ const getHomePage = app.get('/', async (request, response) => {
   }
 })
 
-const getAllBlogs = app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+const getAllBlogs = app.get('/api/blogs', async (request, response) => {
+  try {
+    const blogs = await Blog.find({});
+    response.json(blogs);
+  }
+  catch (error) {
+    console.log(error);
+  }
 })
 
-const createBlog = app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+const createBlog = app.post('/api/blogs', async (request, response) => {
+  try {
+    if (!request.body.title || !request.body.author || !request.body.url) {
+      return response.status(400).json({ error: 'missing title, author or url' })
+    }
+    const blog = new Blog(request.body)
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  }
+  catch (error) {
+    console.log(error);
+  }
 })
 
 module.exports = {
